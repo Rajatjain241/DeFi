984af3cd1eefa8a384c1b6a87c8940a08fd722b3
Rajat Jain <jrajat72@gmail.com>
2023-01-30 23:13:18 +0530

    Added Flashloan

---
+++ FlashLoan/scripts/deploy.ts
@@ -0,0 +1,17 @@
+import hre from "hardhat";
+
+async function main() {
+  const FlashLoan = await hre.ethers.getContractFactory("FlashLoan");
+  const flashloan = await FlashLoan.deploy(
+    "0xc4dCB5126a3AfEd129BC3668Ea19285A9f56D15D"
+  );
+
+  await flashloan.deployed();
+
+  console.log("The address of the deployed contract is : ", flashloan.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
